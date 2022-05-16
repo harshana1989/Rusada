0f2ee892c206eb74b10123d3c954a6b1f1849dec
harshana <lhhmadusanka@gmail.com>
2022-05-16 18:59:42 +0530

    db

--- RusdaDatabase/dbo/Tables/Spotter.sql
+++ RusdaDatabase/dbo/Tables/Spotter.sql
@@ -6,6 +6,7 @@
 	[Registration] nvarchar(7),
 	[Location] nvarchar(255),
 	[Date] datetime,
+	[IsActive] bit DEFAULT 'True',
 	constraint FK_MakeId Foreign Key (MakeId) References Make(Id),
 	constraint FK_AirlineModelId Foreign Key ([ModelId]) References AirlineModel(Id)
 )
